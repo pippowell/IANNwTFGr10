caa40d1bc8686b9ce122f472da84a82f312adfd3
wlee <prizle858@gmail.com>
2022-10-28 15:26:52 +0200

    3

---
+++ Homework/week1/3.math.py
@@ -0,0 +1,15 @@
+from sympy import symbols, diff, exp
+# sympy is used to calculate the partial derivative in a mathematical function.
+
+# define sigmoid function
+def sigmoid(x):
+    return 1 / (1 + exp(-x))
+
+# symbol() function's argument is a string containing symbol which can be assigned to a variable
+a, b, x, z = symbols("a, b, x, z", real = True) 
+f = 4*a*x**2 + a + 3 + sigmoid(z) + (sigmoid(b))**2
+
+print(diff(f, x))
+print(diff(f, z))
+print(diff(f, a))
+print(diff(f, b))
