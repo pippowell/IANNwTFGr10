04fcc44169f73f54d8dacd9efbdab174e29ec456
Piper Powell <ppowell@uni-osnabrueck.de>
2022-11-06 15:50:22 +0100

    add start of piper input hw2

--- Homework/02/2.Perceptrons/dataset.py
+++ Homework/02/2.Perceptrons/dataset.py
@@ -2,8 +2,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # 1. Randomly generate 100 numbers between 0 and 1 and save them to an array ’x’. These are your input values.
+# DECISION NEEDED: Float or integer numbers here?
 x = np.random.random_sample((100))
 print(x)
+
+#Alternatives
+# x = np.random.uniform(0,1,size=100)
+# x = np.random.sample(100)
+# if integer x = np.random.randint(low=0,high=1,size=100)
 
 # # 2. Create an array ’t’. For each entry x[i] in x, calculate x[i]**3-x[i]**2 and save the results to t[i]. These are your targets.
 t = []
@@ -11,6 +17,10 @@ for element_x in range(0, len(x)):
     element_t = x[element_x]**3 - x[element_x]**2 
     t.append(element_t)
 print(t)
+
+#Alternative - not sure if works
+#test = x**3-x**2
+#print(test)
 
 # Optional: Plot your data points along with the underlying function which generated them. 
 # Wooki: not sure what they mean by "along with the underlying function which generated them"
