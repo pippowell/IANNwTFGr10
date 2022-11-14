e766fe8f30076f8ef28b3d7075262f4c24046d63
wlee <prizle858@gmail.com>
2022-11-14 15:51:01 +0100

    Update mnisttf.py

--- Homework/03/mnisttf.py
+++ Homework/03/mnisttf.py
@@ -35,6 +35,28 @@ print("prepare train_ds", train_ds.apply(prepare_mnist_data))
 print("prepare test_ds", test_ds.apply(prepare_mnist_data))
 
 # 2.3 Building a deep neural network with TensorFlow
+class Dense(tf.keras.layers.Layer):
 
-    
+    def __init__(self, n_units, activation_function, **kwargs):
+        super(Dense, self).__init__(**kwargs)
+
+        self.n_units = n_units
+        self.activation_function = activation_function
+        
+    def build(self, input_shape):
+        self.w = tf.Variable(tf.random.normal([input_shape[-1], self.n_units]), name='weights')
+        self.b = tf.Variable(tf.zeros([self.n_units]), name='bias')
+
+    def call(self, inputs):
+        x = inputs @ self.w + self.b
+        return self.activation_function(x)
+
+# good (albeit arbitrary) starting point would be to have two hidden layers with 256 units each
+hidden_layer_1 = Dense(n_units=256, activation_function=tf.nn.relu)
+hidden_layer_2 = Dense(n_units=256, activation_function=tf.nn.relu)
+output_layer = Dense(n_units=9, activation_function=tf.nn.softmax)
+
+# 2.4 Training the network
+
+
 
