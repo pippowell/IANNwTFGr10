e9387b745759ee68ddadd329f94fd62812560aca
wlee <prizle858@gmail.com>
2022-11-14 15:07:33 +0100

    Update mnisttf.py

--- Homework/03/mnisttf.py
+++ Homework/03/mnisttf.py
@@ -2,9 +2,9 @@ import tensorflow_datasets as tfds
 import tensorflow as tf
 
 #2.1 Load Dataset
-(train_ds,test_ds),ds_info = tfds.load ('mnist', split =[ 'train','test'], as_supervised = True, with_info = True)
+(train_ds, test_ds),ds_info = tfds.load ('mnist', split =['train', 'test'], as_supervised = True, with_info = True)
 
-print(ds_info)
+# print("ds_info: \n", ds_info)
 
 #Dataset Info
 #Number of Training Images: 60000
@@ -12,7 +12,7 @@ print(ds_info)
 #Image Shape: 28,28,1 (28 by 28 images, grayscale)
 #Range of Pixels: 0-255 (grayscale)
 
-tfds.show_examples(train_ds,ds_info)
+# tfds.show_examples(train_ds, ds_info)
 
 #2.2 Data Pipeline
 
