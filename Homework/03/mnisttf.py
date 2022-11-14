859f86ac3dabfd431e8bf10d139ed9327e9c0ef6
wlee <prizle858@gmail.com>
2022-11-14 15:58:48 +0100

    Update mnisttf.py

--- Homework/03/mnisttf.py
+++ Homework/03/mnisttf.py
@@ -54,7 +54,9 @@ class Dense(tf.keras.layers.Layer):
 # good (albeit arbitrary) starting point would be to have two hidden layers with 256 units each
 hidden_layer_1 = Dense(n_units=256, activation_function=tf.nn.relu)
 hidden_layer_2 = Dense(n_units=256, activation_function=tf.nn.relu)
-output_layer = Dense(n_units=9, activation_function=tf.nn.softmax)
+
+# 10 units since we need to categorize 9 numbers: 0 - 9, softmax as activation function due to its output as a vector, unlike sigmoid
+output_layer = Dense(n_units=10, activation_function=tf.nn.softmax)
 
 # 2.4 Training the network
 
