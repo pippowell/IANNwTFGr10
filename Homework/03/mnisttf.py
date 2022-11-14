e4c0471cff08772282c2e87402d8e783193b1648
Piper Powell <ppowell@uni-osnabrueck.de>
2022-11-14 15:48:26 +0100

    continue hw 3

--- Homework/03/mnisttf.py
+++ Homework/03/mnisttf.py
@@ -1,5 +1,6 @@
 import tensorflow_datasets as tfds
 import tensorflow as tf
+import numpy as np
 
 #2.1 Load Dataset
 (train_ds,test_ds),ds_info = tfds.load ('mnist', split =[ 'train','test'], as_supervised = True, with_info = True)
@@ -16,4 +17,29 @@ tfds.show_examples(train_ds,ds_info)
 
 #2.2 Data Pipeline
 
+def prepare_data(dataset):
+    # convert data from uint8 to float32
+    dataset = dataset.map(lambda img, target: (tf.cast(img, tf.float32), target))
 
+    #flatten the images into vectors
+    dataset = dataset.map(lambda img, target: (tf.reshape(img, (-1,)), target))
+
+    #input normalization, just bringing image values from range [0, 255] to [-1, 1]
+    dataset = dataset.map(lambda img, target: ((img / 128.) - 1., target))
+
+    #create one-hot targets
+    dataset = dataset.map(lambda img, target: (img, tf.one_hot(target, depth=10)))
+
+    # cache
+    dataset = dataset.cache()
+
+    # shuffle, batch, prefetch
+    dataset = dataset.shuffle(1000)
+    dataset = dataset.batch(32)
+    dataset = dataset.prefetch(20)
+
+    # return preprocessed dataset
+    return dataset
+
+train_dataset = train_ds.apply(prepare_data)
+test_dataset = test_ds.apply(prepare_data)
