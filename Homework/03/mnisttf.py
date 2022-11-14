15c9c8448c3fb686673a998d6275593652761939
Piper Powell <ppowell@uni-osnabrueck.de>
2022-11-14 11:53:20 +0100

    begin hw 3

---
+++ Homework/03/mnisttf.py
@@ -0,0 +1,19 @@
+import tensorflow_datasets as tfds
+import tensorflow as tf
+
+#2.1 Load Dataset
+(train_ds,test_ds),ds_info = tfds.load ('mnist', split =[ 'train','test'], as_supervised = True, with_info = True)
+
+print(ds_info)
+
+#Dataset Info
+#Number of Training Images: 60000
+#Number of Testing Images: 10000
+#Image Shape: 28,28,1 (28 by 28 images, grayscale)
+#Range of Pixels: 0-255 (grayscale)
+
+tfds.show_examples(train_ds,ds_info)
+
+#2.2 Data Pipeline
+
+
