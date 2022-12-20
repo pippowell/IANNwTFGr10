c9387119c3e5a8ae8b96861d6340576f30b57d45
Milad <miladrouygari@gmail.com>
2022-12-20 16:04:57 +0100

    trying to add new target

--- Homework/07/dataset.py
+++ Homework/07/dataset.py
@@ -1,10 +1,21 @@
+import tensorflow_datasets as tfds
+import tensorflow as tf
+import numpy as np
+
+
+# 2.1 Load Dataset
+(train_ds, test_ds), ds_info = tfds.load ('mnist', split =['train', 'test'], as_supervised = True, with_info = True)
+
+# print("ds_info: \n", ds_info)
+tfds.show_examples(train_ds, ds_info)
+
 
 def preprocess(dataset, batchsize, sequence_len):
     '''
     :param dataset: the dataset to be prepared for input into the network
     :param batchsize: the desired batchsize
-    :return: 2 datasets, one each for each of the math problems defined (see below), created after the original database was preprocessed with the
-    steps below
+    :param sequence_len: sequence the dataset (you may think this as a another batch)
+    :return: sequenced dataset with sequential target
     '''
 
     # Step 1 - General Preprocessing
@@ -12,50 +23,44 @@ def preprocess(dataset, batchsize, sequence_len):
     # convert data from uint8 to float32
     dataset = dataset.map(lambda img, target: (tf.cast(img, tf.float32), target))
 
-    # flatten the images into vectors
-    # dataset = dataset.map(lambda img, target: (tf.reshape(img, (-1,)), target))
-
     # input normalization, just bringing image values from range [0, 255] to [-1, 1]
     dataset = dataset.map(lambda img, target: ((img / 128.) - 1., target))
-
-    # data = tf.data.Dataset.zip((dataset.shuffle(2000), dataset.shuffle(2000)))
 
     # print(dataset.shape)
     # The output of that lambda function should be a tuple of two tensors of shapes (num_images, height, width, 1) and (num_images, 1) or (num_images,)
 
-    # Step 2 - Pairing Data Tuples & Respective Parameterized Targets
 
-    # create a dataset that contains 2000 samples from the overall dataset paired with 2000 other samples
-    # data = tf.data.Dataset.zip((dataset.shuffle(2000), dataset.shuffle(2000), dataset.shuffle(2000), dataset.shuffle(2000)))
-
-    # create the dataset for the first math problem (a + b >= 5) - remembering to cast to int versus boolean!
-    # first = data.map(lambda x1, x2, x3, x4: (x1[0], x2[0], x3[0], x4[0], x1[1]))
-    # second = data.map(lambda x1, x2, x3, x4: (x1[0], x2[0], x3[0], x4[0], x1[1] - x2[1]))
-    # third = data.map(lambda x1, x2, x3, x4: (x1[0], x2[0], x3[0], x4[0], x1[1] - x2[1] + x3[1]))
-    # fourth = data.map(lambda x1, x2, x3, x4: (x1[0], x2[0], x3[0], x4[0], x1[1] - x2[1] + x3[1] - x4[1]))
-
-    # list = [first, second, third, fourth]
-
-    # dataset = dataset.map(lambda img, target: img, new_target_fnc(target, sequence_len))
-
-    # Step 3 - Batching & Prefetching
-    # new = new_target_fnc(data, sequence_len)
+    # Step 3 - Sequence Batching & Batching & Prefetching
     dataset = dataset.batch(sequence_len)
 
-    # labels = np.concatenate([label for img, label in dataset], axis=0)
-    labels = [labels for _, labels in dataset.unbatch()]
-    new_labels = new_target_fnc(labels, sequence_len)
+    # change the target
 
-    # dataset = dataset.map(lambda img, target: img, new_labels)
-    dataset = tf.data.Dataset.zip(dataset, new_labels)
-    # dataset = dataset.map(lambda img, target1, target2: img, target1))
-    # dataset = dataset.map(lambda img, target: img, tf.data.Dataset.from_tensor_slices(target))
+    # alternate positive, negative target values
+    range_vals = tf.range(sequence_len)
+    dataset = dataset.map(lambda img, target:
+                          img, tf.from_tensor_slices(target))
 
-    # create a dataset that contains 2000 samples from the overall dataset paired with 2000 other samples
-    # dataset = tf.data.Dataset.zip((dataset.shuffle(2000), dataset.shuffle(2000)))
+    dataset = dataset.map(lambda img, target:
+                          img, tf.where(tf.math.floormod(range_vals,2)==0, target, -target))
 
-    dataset = dataset.batch(batchsize)
-    dataset = dataset.prefetch(tf.data.AUTOTUNE)
+    dataset = dataset.map(lambda img, target:
+                          img, (tf.math.cumsum(target)))
+
+    #dataset = dataset.batch(batchsize)
+    #dataset = dataset.prefetch(tf.data.AUTOTUNE)
 
     # The shape of your tensors should be (batch, sequence-length, features).
     return dataset
+
+# Test
+
+train_ds = preprocess(train_ds, 32, 4)
+test_ds = preprocess(test_ds, 32, 4)
+
+# print(type(train_ds))
+
+for img, label in train_ds.take(1):
+    print(img, label)
+
+# (bs, num_images, height, width, 1)
+# (bs, num_images, 1)
