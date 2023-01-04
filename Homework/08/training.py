28cb3764cdb9440416474e0a92d60a033f1d4410
wlee <prizle858@gmail.com>
2023-01-04 15:05:44 +0100

    Update training.py

--- Homework/08/training.py
+++ Homework/08/training.py
@@ -4,6 +4,7 @@ import model
 import matplotlib.pyplot as plt
 import datetime as datetime
 from pathlib import Path
+import numpy as np
 
 # Initiate epochs and learning rate as global variables
 epochs = 2 # 10
@@ -23,15 +24,21 @@ ae.compile(loss=loss, optimizer=opti)
 # test_x, _ = dataset.test_ds
 
 noisy_train_x = list()
-train_x = list()
 noisy_test_x = list()
+train_x = list()
 test_x = list()
 
 for x, _ in dataset.noisy_train_ds: noisy_train_x.append(x)
-for x, _ in dataset.train_ds: train_x.append(x)
-
 for x, _ in dataset.noisy_test_ds: noisy_test_x.append(x)
+for x, _ in dataset.train_ds: train_x.append(x)
 for x, _ in dataset.test_ds: test_x.append(x)
-    
+
+# print(f"noisy_train_x: {len(noisy_train_x)}")
+# print(f"train_x: {len(train_x)}")
+# print(f"noisy_test_x: {len(noisy_test_x)}")
+# print(f"test_x: {len(test_x)}")
+
 ae.fit(x=noisy_train_x, y=train_x, validation_data=(noisy_test_x, test_x), epochs=epochs)
+# this part not working!!
+
 print("training done")
