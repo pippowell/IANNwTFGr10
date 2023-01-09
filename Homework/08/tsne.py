63e35e51e7898f2d7cc91823ecd7976beb8b9404
wlee <prizle858@gmail.com>
2023-01-09 22:38:35 +0100

    Update tsne.py

--- Homework/08/tsne.py
+++ Homework/08/tsne.py
@@ -4,23 +4,32 @@ import matplotlib.pyplot as plt
 from sklearn.manifold import TSNE
 import pandas as pd
 import seaborn as sns
+from keras.datasets import mnist
 
-ae_reload = tf.keras.models.load_model('Homework/08/my_ae')
+ae_reload = tf.keras.models.load_model('Homework/08/my_model:e=15,lr=1e-3')
 
 tsne = TSNE(n_components=2)
 
-test_1000 = dataset.noisy_img_train[:1000]
-y = dataset.labels_test
+# test_1000 = dataset.noisy_img_train[:1000]
+# test_ds = dataset.noisy_img_train.with_format("tf")
+# y = dataset.labels_test
 
-ae_encoding = ae_reload.encoder(test_1000)
+(_, _), (test_X, test_Y) = mnist.load_data()
+test_X = tf.expand_dims(test_X, axis=-1)
+test_X = tf.cast(test_X, tf.float32)
+test_X = test_X[:1000]
 
-tsne_result = tsne.fit_transform(ae_encoding)
+ae_encoder = ae_reload.encoder
+
+output_of_encoder = ae_encoder(test_X, False)
+
+tsne_result = tsne.fit_transform(output_of_encoder)
 
 # Plot the result of our TSNE with the label color coded
-tsne_result_df = pd.DataFrame({'tsne_1': tsne_result[:,0], 'tsne_2': tsne_result[:,1], 'label': y[:1000]})
+tsne_result_df = pd.DataFrame({'tsne_1': tsne_result[:,0], 'tsne_2': tsne_result[:,1], 'label': test_Y[:1000]})
 
 plt.figure(figsize=(10,8))
 sns.scatterplot(x='tsne_1', y='tsne_2', hue='label', data=tsne_result_df)
-plt.title("t-SNE with mnist_784")
-plt.savefig("Homework/08/plot/t-sne_mnist784")
+plt.title("t-SNE with 1000 testds")
+plt.savefig("Homework/08/plot/tsne_1000testds")
 plt.show()
